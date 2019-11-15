9a20af2a52ca71e52d7acce8b6c1023e5e215b6d
Deehoos <dhughes238x@gmail.com>
2019-11-15 16:04:24 -0600

    work on dbcontexT

---
+++ OfficeTracker/ClientApp/src/OfficeTrackerContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using OfficeTracker.ClientApp.src.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OfficeTracker.ClientApp.src
+{
+  public class OfficeTrackerContext : DbContext
+  {
+    public OfficeTrackerContext(DbContextOptions<OfficeTrackerContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Office> Offices { get; set; }
+    public DbSet<Manager> Managers { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+  }
+}
--- OfficeTracker/Program.cs
+++ OfficeTracker/Program.cs
@@ -5,7 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using OfficeTracker.ClientApp.src;
 
 namespace OfficeTracker
 {
@@ -13,7 +16,30 @@ namespace OfficeTracker
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            IWebHost host = CreateWebHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+
+            host.Run();
+        }
+
+        private static void CreateDbIfNotExists(IWebHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<OfficeTrackerContext>();
+                    context.Database.EnsureCreated();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
